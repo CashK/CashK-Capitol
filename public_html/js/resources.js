f2038c68e7b8227b0f1d839a20521776a4295b32
Kobi Cash <bluetorrent6@gmail.com>
2014-12-15 10:31:20 -0800

    stuff

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -9,7 +9,7 @@ game.resources = [
         {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
         {name: "hero1", type:"image", src: "data/img/hero1.png"},
         {name: "title-screen", type:"image", src: "data/img/title-screen.png"},
-        {name: "slime", type:"image", src: "data/img/slime-.png"},
+        {name: "slime", type:"image", src: "data/img/slime-spritesheet.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -21,8 +21,9 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
         //call the game maps to be recognized by the program
-        {name: "Stage01", type: "tmx", src: "data/map/Stage02.tmx"},
-        {name: "Stage02", type: "tmx", src: "data/map/Stage03.tmx"},
+        {name: "Stage01", type: "tmx", src: "data/map/Stage01.tmx"},
+        {name: "Stage02", type: "tmx", src: "data/map/Stage02.tmx"},
+        {name: "Stage03", type: "tmx", src: "data/map/Stage03.tmx"},
         {name: "Stage04", type: "tmx", src: "data/map/Stage04.tmx"}
 
 	/* Background music. 
--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -30,6 +30,6 @@ game.PlayScreen = me.ScreenObject.extend({
         
         resetPlayer: function(x, y){
             var player = me.pool.pull("hero1", x, y, {});
-            me.game.world.addChild(player, 3);
+            me.game.world.addChild(player, 5);
         }
 });
